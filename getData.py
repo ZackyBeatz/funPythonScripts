9fa8fc2ab7357ab5ca3dd5168aa1bf9ffce316c1
Zakaria Ahmed <zacky89@hotmail.com>
2017-03-22 13:59:34 +0100

    script that crawls through a webDoc

--- getData.py
+++ getData.py
@@ -1,9 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 
+#  FUN SCRIPT THAT CRAWLS THROUGH A WEB DOCUMENT WITH BEAUTIFULSOUP
+# check BeautifulSoup Docs for all information
 r = requests.get("http://example.com/")
 
 soup = BeautifulSoup(r.text, 'lxml')
 
 
-print(soup.prettify())
+
+# gets all a href links
+allLinks = soup.find_all('a')
+
+# gets h1 within body 
+head1 = soup.body.h1
+
+# gets same h1 without title attribute
+headWTitle = soup.body.h1.string
+
+
+
+# gets document title
+getDocumentTitle = soup.title
+
+
+print(headWTitle)
